26277d349b23eeb6d6ba7a983ae5bd933e062ac1
mindr17 <mindr17@yandex.ru>
2023-11-16 18:53:08 +0300

    feat: add eslint

--- src/app/nested/layout.tsx
+++ src/app/nested/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,9 +15,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}
       <div>test layout</div>
-      </body>
+      <div>
+        {children}
+      </div>
     </html>
   )
 }
---
+++ src/app/nested/page.tsx
@@ -0,0 +1,5 @@
+export default function Nested() {
+  return (
+    <div>nested</div>
+  )
+}
