a82de0f370ef81bd4c426855743340697aa0e8c4
mindr17 <mindr17@yandex.ru>
2023-11-17 15:40:20 +0300

    feat: add meta

--- src/shared/home-stats/home-stats.tsx
+++ src/shared/home-stats/home-stats.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { formatNumberWithCommas } from '@/helpers/helpers';
 import {
   iconsStatsHtml,
   iconsTotalBadges,
@@ -9,22 +10,25 @@ import Icon from '@/shared/icon/icon';
 import s from './home-stats.module.css';
 
 export default function HomeStats(): JSX.Element {
-  const clubsCount = 2904;
+  const badgesCount = 2904;
+  const grabbedCount = 291234;
 
   return (
     <div className={s.container}>
-      <div className={`${s.link} ${s.cities}`}>
-        {/* <div className={`${s.h3} ${s.green}`}>{clubsCountTitle}</div> */}
-        <div className={`${s.h3} ${s.green}`}>Badges in library</div>
-        <div className={s.count}>{clubsCount}</div>
+      <div className={`${s.link} ${s.clubs}`}>
+        <div className={`${s.h3} ${s.red}`}>Badges in library</div>
+        <div className={s.count}>
+          {formatNumberWithCommas(badgesCount)}
+        </div>
         <div className={s.icon}>
           <Icon html={iconsTotalBadges} />
         </div>
       </div>
       <div className={`${s.link} ${s.cities}`}>
-        {/* <div className={`${s.h3} ${s.green}`}>{clubsCountTitle}</div> */}
-        <div className={`${s.h3} ${s.green}`}>Badges in library</div>
-        <div className={s.count}>{clubsCount}</div>
+        <div className={`${s.h3} ${s.green}`}>Badges grabbed</div>
+        <div className={s.count}>
+          {formatNumberWithCommas(grabbedCount)}
+        </div>
         <div className={s.icon}>
           <Icon html={iconsStatsHtml} />
         </div>
