4d82de61a9a74c26395e5831526d0b6417644395
mindr17 <mindr17@yandex.ru>
2023-11-17 14:00:34 +0300

    fix: hero img

--- src/shared/hero/hero.tsx
+++ src/shared/hero/hero.tsx
@@ -1,8 +1,19 @@
+import Image from 'next/image';
+
 import s from './hero.module.css';
 
-export default function BadgeProduct(): JSX.Element {
+export default function Hero(): JSX.Element {
   return (
     <div className={s.container}>
+      <div className={s.heroImgContainer}>
+        <Image
+          alt={'badges'}
+          className={s.heroImg}
+          fill
+          priority
+          src={'/hero.jpg'}
+        />
+      </div>
       <div className={s.h1}>Get beautiful badges in a few clicks</div>
       <div className={s.description}>
         For Github profile, personal website, etc
