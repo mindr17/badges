5be7315fb154058ed00c60d1b04af71d7a3d835f
mindr17 <mindr17@yandex.ru>
2023-11-17 14:59:16 +0300

    fix: stats

--- src/shared/home-stats/home-stats.tsx
+++ src/shared/home-stats/home-stats.tsx
@@ -1,95 +1,31 @@
-import Link from 'next/link';
+import { clsx } from 'clsx';
 import React from 'react';
 
 import { getWordCase } from '@/helpers/get-word-case';
+import { iconsStatsHtml } from '@/iconsHtml/iconsHtml';
+import Icon from '@/shared/icon/icon';
 
-// import { Icon, iconsHtml } from '@/src/components/icons/icons';
 import s from './home-stats.module.css';
 
-interface Props {}
-
-const HomeStats = ({ data }: Props) => {
-  const citiesCountTitle = getWordCase(2904, [
-    'Город',
-    'Города',
-    'Городов',
+const HomeStats = () => {
+  const clubsCount = 2904;
+  const clubsCountTitle = getWordCase(clubsCount, [
+    'Icon',
+    'Icons',
+    'Icons',
   ]);
-  const clubsCountTitle = getWordCase(2904, [
-    'Клуб',
-    'Клуба',
-    'Клубов',
-  ]);
-  const onlineCountTitle = getWordCase(2904, [
-    'Платформа',
-    'Платформы',
-    'Платформ',
-  ]);
-  // const onlineCountTitle = 'Онлайн';
 
   return (
     <div className={s.container}>
       <div className={s.main}>
-        <Link
-          className={classNames(s.link, s.clubs)}
-          href={'/cities'}
-        >
-          <div className={classNames(s.h3, s.red)}>
-            {citiesCountTitle}
-          </div>
-          <p className={s.count}>{citiesCount}</p>
-          <img
-            alt='cities'
-            className={s.icon}
-            src='/icons/home/clubs.svg'
-          />
-        </Link>
-        <Link
-          className={classNames(s.link, s.ratings)}
-          href={'/cities'}
-        >
-          <div className={classNames(s.h3, s.yellow)}>
-            {clubsCountTitle}
-          </div>
-          <p className={s.count}>{clubsCount}</p>
+        <div className={clsx(s.link, s.cities)}>
+          <div className={clsx(s.h3, s.green)}>{clubsCountTitle}</div>
+          <div className={s.count}>{clubsCount}</div>
           <div className={s.icon}>
-            <Icon html={iconsHtml.people} />
+            <Icon html={iconsStatsHtml} />
           </div>
-        </Link>
-        <Link
-          className={classNames(s.link, s.cities)}
-          href={'/online'}
-        >
-          <div className={classNames(s.h3, s.green)}>
-            {onlineCountTitle}
-          </div>
-          <p className={s.count}>
-            {data?.online_aggregated?.[0]?.count?.id ?? 0}
-          </p>
-          <img
-            alt='online'
-            className={s.icon}
-            src='/icons/home/cities.svg'
-          />
-        </Link>
+        </div>
       </div>
-      {/* <ul className={s.additional}>
-        <li className={s.item}>
-          <p className={s.text}>Игры</p>
-          <img src='/icons/home/frame.svg' alt='frame' className={s.img} />
-        </li>
-        <li className={s.item}>
-          <p className={s.text}>Медиа</p>
-          <img src='/icons/home/frame.svg' alt='frame' className={s.img} />
-        </li>
-        <li className={s.item}>
-          <p className={s.text}>Аналитика</p>
-          <img src='/icons/home/frame.svg' alt='frame' className={s.img} />
-        </li>
-        <li className={s.item}>
-          <p className={s.text}>Обучение</p>
-          <img src='/icons/home/frame.svg' alt='frame' className={s.img} />
-        </li>
-      </ul> */}
     </div>
   );
 };
