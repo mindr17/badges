d820523e429b0f3c8a203e964c1f8ed81369abf6
mindr17 <mindr17@yandex.ru>
2023-11-17 10:48:57 +0300

    fix: adobe illustarator

--- src/shared/badge-product/badge-product.tsx
+++ src/shared/badge-product/badge-product.tsx
@@ -26,7 +26,7 @@ export default function BadgeProduct(): JSX.Element {
           alt={''}
           className={s.imgStandard}
           src={
-            'https://img.shields.io/badge/React-61DAFB?style=for-the-badge&logo=React&logoColor=000000'
+            'https://camo.githubusercontent.com/1429d75133a7ceb9bdd2ef4c71dac4771ded646ba9b1ce0eaa1eacd4716a5c55/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f41646f6265253230496c6c7573747261746f722d4646394130303f7374796c653d666f722d7468652d6261646765266c6f676f3d41646f6265253230496c6c7573747261746f72266c6f676f436f6c6f723d303030303030'
           }
         />
       </div>
